7b3e63634c691bd03250c4f540d4814bcb27154c
larizgoitia <larizgoitia@1816c63b-b73a-4b72-92f7-1bc7269d86da>
2010-11-19 15:47:33 +0000

    Refactored with the new hierarchy (XmlHandlerBase.java) Added a method to remove some optional arguments (ordered in args).

--- rif4j-impl/src/main/java/at/sti2/rif4j/parser/xml/XmlParser.java
+++ rif4j-impl/src/main/java/at/sti2/rif4j/parser/xml/XmlParser.java
@@ -18,22 +18,21 @@ package at.sti2.rif4j.parser.xml;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.net.URL;
 import java.util.Collection;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
 
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import at.sti2.rif4j.condition.Formula;
 import at.sti2.rif4j.rule.Document;
+import at.sti2.rif4j.serializer.xml.XmlHandlerBase;
 
 /**
  * Parses RIF BLD documents and formulas represented in the RIF XML syntax. This
@@ -43,17 +42,16 @@ import at.sti2.rif4j.rule.Document;
  * 
  * @author Adrian Marte
  */
-public class XmlParser {
-
-	private boolean isValidating;
-
+public class XmlParser extends XmlHandlerBase
+{
 	/**
 	 * Creates a new parser for RIF XML documents and formulas. By default the
 	 * parser does not validate the XML documents against the XML schema of RIF
 	 * XML.
 	 */
-	public XmlParser() {
-		this(false);
+	public XmlParser()
+	{
+		super();
 	}
 
 	/**
@@ -61,31 +59,32 @@ public class XmlParser {
 	 * value of <code>isValidating</code> the parser also validates the XML
 	 * document against the XML schemo of the RIF XML syntax.
 	 * 
-	 * @param isValidating If set to <code>true</code>, the parser first
-	 *            validates the specified XML document against the XML schemo of
-	 *            RIF XML.
+	 * @param useValidation
+	 *            If set to <code>true</code>, the parser first validates the
+	 *            specified XML document against the XML schemo of RIF XML.
 	 */
-	public XmlParser(boolean isValidating) {
-		this.isValidating = isValidating;
+	public XmlParser(boolean useValidation)
+	{
+		super(useValidation);
 	}
 
 	/**
 	 * Parses a RIF Document using the specified reader.
 	 * 
-	 * @param reader The reader which serves the document.
+	 * @param reader
+	 *            The reader which serves the document.
 	 * @return A Document object representing the specified RIF document.
-	 * @throws SAXException If a SAX error or warning occurs.
-	 * @throws IOException If an I/O error or waring occurs.
-	 * @throws ParserConfigurationException Indicates a configuration error.
+	 * @throws SAXException
+	 *             If a SAX error or warning occurs.
+	 * @throws IOException
+	 *             If an I/O error or waring occurs.
+	 * @throws ParserConfigurationException
+	 *             Indicates a configuration error.
 	 */
-	public Document parseDocument(Reader reader) throws SAXException,
-			IOException, ParserConfigurationException {
+	public Document parseDocument(Reader reader) throws SAXException, IOException, ParserConfigurationException
+	{
 		org.w3c.dom.Document xmlDocument = parseXml(reader);
-
-		if (isValidating) {
-			validate(xmlDocument);
-		}
-
+		
 		XmlExtractor extractor = new XmlExtractor();
 		Document document = extractor.extractDocument(xmlDocument);
 
@@ -95,70 +94,64 @@ public class XmlParser {
 	/**
 	 * Parses a RIF Formula using the specified reader.
 	 * 
-	 * @param reader The reader which serves the formula.
+	 * @param reader
+	 *            The reader which serves the formula.
 	 * @return A Formula object representing the specified RIF formula.
-	 * @throws SAXException If a SAX error or warning occurs.
-	 * @throws IOException If an I/O error or waring occurs.
-	 * @throws ParserConfigurationException Indicates a configuration error.
+	 * @throws SAXException
+	 *             If a SAX error or warning occurs.
+	 * @throws IOException
+	 *             If an I/O error or waring occurs.
+	 * @throws ParserConfigurationException
+	 *             Indicates a configuration error.
 	 */
-	public Formula parseFormula(Reader reader)
-			throws ParserConfigurationException, SAXException, IOException {
+	public Formula parseFormula(Reader reader) throws ParserConfigurationException, SAXException, IOException
+	{
 		org.w3c.dom.Document xmlDocument = parseXml(reader);
-
-		if (isValidating) {
-			validate(xmlDocument);
-		}
-
+		
 		XmlExtractor extractor = new XmlExtractor();
 		Collection<Formula> formulas = extractor.extractFormulas(xmlDocument);
-
-		if (formulas.size() > 0) {
+		
+		if (formulas.size() > 0)
+		{
 			return formulas.iterator().next();
 		}
 
 		return null;
 	}
 
-	static final String JAXP_SCHEMA_LANGUAGE = "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
-
-	static final String W3C_XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
-
-	private org.w3c.dom.Document parseXml(Reader reader)
-			throws ParserConfigurationException, SAXException, IOException {
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		factory.setNamespaceAware(true);
-
-		try {
-			factory.setAttribute(JAXP_SCHEMA_LANGUAGE, W3C_XML_SCHEMA);
-		} catch (IllegalArgumentException x) {
-			// Happens if the parser does not support JAXP 1.2
-		}
-
+	public org.w3c.dom.Document parseXml(Reader reader) throws ParserConfigurationException, SAXException, IOException
+	{
 		InputSource source = new InputSource(reader);
-
-		DocumentBuilder builder = factory.newDocumentBuilder();
+		
+		DocumentBuilder builder = this.getDocumentBuilder();		
 		org.w3c.dom.Document xmlDocument = builder.parse(source);
 
+		if (useValidation)
+		{
+			this.validate(xmlDocument,BLD_RULE_XSD);
+		}
+		
+		this.removeOrderedArgsAttribute(xmlDocument);
+		
 		return xmlDocument;
 	}
 
-	private void validate(org.w3c.dom.Document document) throws SAXException,
-			IOException {
-		SchemaFactory factory = SchemaFactory
-				.newInstance("http://www.w3.org/2001/XMLSchema");
-
-		// Its enough to load the BLD Rule Schema.
-		// URL condSchemaUrl = openSchema("BLDCond.xsd");
-		URL ruleSchemaUrl = openSchema("BLDRule.xsd");
-
-		Schema schema = factory.newSchema(ruleSchemaUrl);
-		Validator validator = schema.newValidator();
-		validator.validate(new DOMSource(document));
-	}
-
-	private URL openSchema(String fileName) {
-		URL url = getClass().getClassLoader().getResource(fileName);
-		return url;
-	}
-
+	/**
+	 * Removes the attribute ordered from xml elements in a given xml document
+	 * This attribute is optional and has a fixed value, so it is not necessary
+	 * and it is simpler not to have it (and better for testing purposes)
+	 * 
+	 * @param xmlDocument The xml document from which to remove the ordered attribute in the args element
+	 */
+	private void removeOrderedArgsAttribute(org.w3c.dom.Document xmlDocument)
+	{
+		NodeList nodeList = xmlDocument.getElementsByTagName("args");		
+		for (int i = 0; i < nodeList.getLength(); i++)
+		{
+			Node node = nodeList.item(i);
+			NamedNodeMap attributes = node.getAttributes();
+			if (attributes.getNamedItem("ordered") != null)
+				attributes.removeNamedItem("ordered");		  
+		}		
+	}	
 }
