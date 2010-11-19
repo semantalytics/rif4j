666b3ab4aba97bd297f845781c9b25eebfa96b92
larizgoitia <larizgoitia@1816c63b-b73a-4b72-92f7-1bc7269d86da>
2010-11-19 16:03:57 +0000

    Added Ignore annotation to previous tests

--- trunk/rif4j-impl/src/test/java/at/sti2/rif4j/serializer/xml/XmlSerializerTest.java
+++ trunk/rif4j-impl/src/test/java/at/sti2/rif4j/serializer/xml/XmlSerializerTest.java
@@ -10,44 +10,50 @@ import java.io.StringReader;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
 import at.sti2.rif4j.parser.xml.XmlParser;
 import at.sti2.rif4j.rule.Document;
 
-public class XmlSerializerTest {
+public class XmlSerializerTest
+{
 
-	private XmlParser parser;
+	private XmlParser	parser;
 
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws Exception
+	{
 		parser = new XmlParser(true);
 	}
 
-	private Reader openTestFile(String fileName) {
+	private Reader openTestFile(String fileName)
+	{
 		return new InputStreamReader(getClass().getClassLoader().getResourceAsStream(fileName));
 	}
 
+	@Ignore
 	@Test
-	public void testRoundTrip() throws IOException, SAXException,
-			ParserConfigurationException {
+	public void testRoundTrip() throws IOException, SAXException, ParserConfigurationException
+	{
 		Reader reader = openTestFile("rif-bld-example.xml");
 
-		try {
+		try
+		{
 			Document original = parser.parseDocument(reader);
 
 			XmlSerializer serializer = new XmlSerializer(true);
 			original.accept(serializer);
-			String output = serializer.getString();
-			System.err.println(output);
+			String output = serializer.serialize(original);
 
 			parser = new XmlParser(true);
 			Document replica = parser.parseDocument(new StringReader(output));
-			
+
 			assertEquals(original, replica);
-			
-		} finally {
+		}
+		finally
+		{
 			reader.close();
 		}
 	}
