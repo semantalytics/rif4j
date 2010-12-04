486b91ea617a85cf4079e2ea311bb1526a4c295c
adrian_marte <adrian_marte@1816c63b-b73a-4b72-92f7-1bc7269d86da>
2010-12-04 23:02:33 +0000

    Extended implementation of the transformer from RIF to IRIS.

--- rif4j-iris/src/main/java/at/sti2/rif4j/translator/iris/RifToIrisVisitor.java
+++ rif4j-iris/src/main/java/at/sti2/rif4j/translator/iris/RifToIrisVisitor.java
@@ -15,30 +15,42 @@
  */
 package at.sti2.rif4j.translator.iris;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.deri.iris.api.basics.IAtom;
 import org.deri.iris.api.basics.IPredicate;
 import org.deri.iris.api.basics.IRule;
+import org.deri.iris.api.basics.ITuple;
+import org.deri.iris.api.terms.ITerm;
+import org.deri.iris.api.terms.IVariable;
+import org.deri.iris.factory.Factory;
 import org.deri.iris.storage.IRelation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import at.sti2.rif4j.condition.AndFormula;
 import at.sti2.rif4j.condition.Argument;
 import at.sti2.rif4j.condition.Atom;
 import at.sti2.rif4j.condition.AtomicFormula;
 import at.sti2.rif4j.condition.AtomicFormulaVisitor;
 import at.sti2.rif4j.condition.CompositeFormula;
+import at.sti2.rif4j.condition.CompositeFormulaVisitor;
 import at.sti2.rif4j.condition.Constant;
 import at.sti2.rif4j.condition.EqualAtom;
 import at.sti2.rif4j.condition.ExistsFormula;
+import at.sti2.rif4j.condition.Expression;
+import at.sti2.rif4j.condition.ExternalExpression;
 import at.sti2.rif4j.condition.ExternalFormula;
 import at.sti2.rif4j.condition.FormulaVisitor;
 import at.sti2.rif4j.condition.Frame;
 import at.sti2.rif4j.condition.MemberAtom;
+import at.sti2.rif4j.condition.OrFormula;
 import at.sti2.rif4j.condition.SubclassAtom;
+import at.sti2.rif4j.condition.TermVisitor;
 import at.sti2.rif4j.condition.Variable;
 import at.sti2.rif4j.rule.Clause;
 import at.sti2.rif4j.rule.ClauseVisitor;
@@ -54,9 +66,11 @@ import at.sti2.rif4j.rule.RuleVisitor;
 
 /**
  * @author Iker Larizgoitia Abad
+ * @author Adrian Marte
  */
 public class RifToIrisVisitor implements DocumentVisitor, AtomicFormulaVisitor,
-		RuleVisitor, FormulaVisitor, ClauseVisitor {
+		RuleVisitor, FormulaVisitor, ClauseVisitor, TermVisitor,
+		CompositeFormulaVisitor {
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(RifToIrisVisitor.class);
@@ -65,9 +79,16 @@ public class RifToIrisVisitor implements DocumentVisitor, AtomicFormulaVisitor,
 
 	private List<IRule> rules;
 
+	private List<ITerm> terms;
+
+	private IAtom atom;
+
 	public RifToIrisVisitor() {
-		this.facts = new HashMap<IPredicate, IRelation>();
-		this.rules = new ArrayList<IRule>();
+		this(null, null);
+	}
+
+	private RifToIrisVisitor(URI baseUri, Map<String, URI> prefixes) {
+		reset();
 	}
 
 	public Map<IPredicate, IRelation> getFacts() {
@@ -78,33 +99,85 @@ public class RifToIrisVisitor implements DocumentVisitor, AtomicFormulaVisitor,
 		return rules;
 	}
 
+	public List<ITerm> getTerms() {
+		return terms;
+	}
+	
+	public IAtom getAtom() {
+		return atom;
+	}
+
+	private void reset() {
+		facts = new HashMap<IPredicate, IRelation>();
+		rules = new ArrayList<IRule>();
+		terms = new ArrayList<ITerm>();
+		atom = null;
+	}
+
+	private static ITerm[] toArray(List<ITerm> terms) {
+		ITerm[] termArray = new ITerm[terms.size()];
+		termArray = terms.toArray(termArray);
+
+		return termArray;
+	}
+
+	private static ITuple toTuple(List<ITerm> terms) {
+		ITerm[] termArray = toArray(terms);
+		ITuple tuple = Factory.BASIC.createTuple(termArray);
+
+		return tuple;
+	}
+
 	@Override
 	public void visit(Document document) {
-		Constant base = document.getBase();
+		// We can ignore the base URI, sinc the names of the predicates, etc.,
+		// should already be absolute URIs.
 
-		Group group = document.getGroup();
-		group.accept(this);
+		// Constant base = document.getBase();
+		// if (base != null && base.getText() != null) {
+		// try {
+		// URI.create(base.getText());
+		// } catch (IllegalArgumentException e) {
+		// logger.error("Base URI is not a valid URI", e);
+		// }
+		// }
 
 		List<Import> imports = document.getImports();
 		for (Import import1 : imports) {
 			import1.accept(this);
 		}
 
-		List<Frame> metadata = document.getMetadata();
-		for (Frame frame : metadata) {
-			// frame.accept(this);
-		}
+		// We can ignore the metadata.
+		// List<Frame> metadata = document.getMetadata();
+		// for (Frame frame : metadata) {
+		// frame.accept((AtomicFormulaVisitor) this);
+		// }
 
 		List<Prefix> prefixes = document.getPrefixes();
 		for (Prefix prefix : prefixes) {
 			prefix.accept(this);
 		}
+
+		Group group = document.getGroup();
+		group.accept(this);
 	}
 
 	@Override
 	public void visit(Group group) {
+		RifToIrisVisitor visitor = new RifToIrisVisitor();
+
 		for (Rule rule : group.getRules()) {
-			rule.accept(this);
+			rule.accept(visitor);
+
+			if (visitor.getRules() != null) {
+				rules.addAll(visitor.getRules());
+			}
+
+			if (visitor.getFacts() != null) {
+				// TODO Merge facts.
+			}
+
+			visitor.reset();
 		}
 	}
 
@@ -115,8 +188,24 @@ public class RifToIrisVisitor implements DocumentVisitor, AtomicFormulaVisitor,
 
 	@Override
 	public void visit(Prefix prefix) {
-		// TODO Auto-generated method stub
-		logger.debug("Visiting prefix");
+		// We can ignore the prefixes, as the names of the predicates etc.
+		// should already be absolute URIs.
+
+		// if (prefixes == null) {
+		// prefixes = new HashMap<String, URI>();
+		// }
+		//
+		// String name = prefix.getName();
+		// Constant iri = prefix.getIri();
+		//
+		// if (name != null && iri != null && iri.getText() != null) {
+		// try {
+		// URI uri = URI.create(iri.getText());
+		// prefixes.put(name, uri);
+		// } catch (IllegalArgumentException e) {
+		// logger.error("Prefix URI is not a valid URI", e);
+		// }
+		// }
 	}
 
 	@Override
@@ -126,11 +215,35 @@ public class RifToIrisVisitor implements DocumentVisitor, AtomicFormulaVisitor,
 		logger.debug("text: ");
 		logger.debug(atom.getOperator().getText());
 
-		List<Argument> args = atom.getArguments();
-		for (Argument argument : args) {
-			// System.out.println(argument.getName());
-			logger.debug(argument.getValue().toString());
+		RifToIrisVisitor visitor = new RifToIrisVisitor();
+		for (Argument argument : atom.getArguments()) {
+			// In IRIS, an argument can not be named, therefore, we can ignore
+			// the name and only use the value.
+			argument.getValue().accept(visitor);
+
+			logger.debug("Argument value is " + argument.getValue().toString());
 		}
+
+		List<ITerm> irisTermList = visitor.getTerms();
+		ITerm[] irisTerms = toArray(irisTermList);
+
+		RifToIrisBuiltinMapper builtInMapper = new RifToIrisBuiltinMapper();
+		IAtom irisAtom = builtInMapper.toIrisBuiltin(atom.getOperator()
+				.getText(), irisTerms);
+
+		// If it is not a built-in, then it is an ordinary predicate.
+		if (irisAtom == null) {
+			String predicateName = atom.getOperator().getText();
+
+			IPredicate predicate = Factory.BASIC.createPredicate(predicateName,
+					irisTerms.length);
+			ITuple tuple = toTuple(irisTermList);
+			irisAtom = Factory.BASIC.createAtom(predicate, tuple);
+			
+			// FIXME What about regular facts?
+		}
+
+		this.atom = irisAtom;
 	}
 
 	@Override
@@ -162,9 +275,14 @@ public class RifToIrisVisitor implements DocumentVisitor, AtomicFormulaVisitor,
 	public void visit(ForallFormula forallFormula) {
 		logger.debug("Visiting forAll");
 
+		RifToIrisVisitor visitor = new RifToIrisVisitor();
+
 		List<Variable> vars = forallFormula.getVariables();
 		for (Variable variable : vars) {
+			variable.accept(visitor);
 		}
+
+		List<ITerm> irisVariables = visitor.getTerms();
 
 		forallFormula.getClause().accept((ClauseVisitor) this);
 	}
@@ -214,6 +332,70 @@ public class RifToIrisVisitor implements DocumentVisitor, AtomicFormulaVisitor,
 		for (AtomicFormula atomicFormula : formulas) {
 			atomicFormula.accept((AtomicFormulaVisitor) this);
 		}
+	}
+
+	@Override
+	public void visit(Constant constant) {
+		if (constant == null) {
+			logger.error("Constant is null");
+			return;
+		}
+
+		RifToIrisConstantMapper mapper = new RifToIrisConstantMapper();
+
+		// FIXME What about the language?
+		ITerm irisTerm = mapper.toIrisTerm(constant.getType(), constant
+				.getText());
+
+		if (irisTerm != null) {
+			terms.add(irisTerm);
+		}
+	}
+
+	@Override
+	public void visit(Expression expression) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(ExternalExpression externalExpression) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(at.sti2.rif4j.condition.List list) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(Variable variable) {
+		if (variable == null) {
+			logger.error("Variable is null");
+			return;
+		}
+
+		IVariable irisVariable = Factory.TERM
+				.createVariable(variable.getName());
+
+		if (irisVariable != null) {
+			terms.add(irisVariable);
+		}
+
+	}
+
+	@Override
+	public void visit(AndFormula andFormula) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(OrFormula orFormula) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
