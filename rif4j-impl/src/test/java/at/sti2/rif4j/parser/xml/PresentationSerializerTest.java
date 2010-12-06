d54f0398b25c03106b7678b6a8b40e8f5a263199
larizgoitia <larizgoitia@1816c63b-b73a-4b72-92f7-1bc7269d86da>
2010-12-06 09:19:01 +0000

    Complete - task 3129461: Remove unnecessary tests https://sourceforge.net/tracker/?group_id=288619&func=detail&atid=1578669&aid=3129461
    Removed old tests.
    Refactored serializers, added a common interface RifSerializer.
    Changed names of the test classes and methods to proper names, matching the class tested.

--- rif4j-impl/src/test/java/at/sti2/rif4j/parser/xml/PresentationSerializerTest.java
+++ rif4j-impl/src/test/java/at/sti2/rif4j/parser/xml/PresentationSerializerTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package at.sti2.rif4j;
+package at.sti2.rif4j.parser.xml;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,29 +24,28 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.apache.commons.io.FileUtils;
 import org.custommonkey.xmlunit.XMLTestCase;
 import org.custommonkey.xmlunit.XMLUnit;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.SAXException;
 
-import at.sti2.rif4j.parser.xml.XmlParser;
+import at.sti2.rif4j.TestUtils;
 import at.sti2.rif4j.rule.Document;
 import at.sti2.rif4j.serializer.presentation.PresentationSerializer;
-import at.sti2.rif4j.serializer.xml.XmlSerializer;
 
-public class RIFParseAndSerializeTest extends XMLTestCase {
+/**
+ * @author Adrian Marte
+ * @author Iker Larizgoitia Abad
+ */
+public class PresentationSerializerTest extends XMLTestCase {
 
 	private static final Logger logger = LoggerFactory
-			.getLogger(RIFParseAndSerializeTest.class);
+	.getLogger(PresentationSerializerTest.class);
 
 	private XmlParser parser = null;
-
-	private XmlSerializer serializer = null;
-
 	private PresentationSerializer presentationSerializer = null;
-
+	
 	@Before
 	public void setUp() throws Exception {
 		XMLUnit.setIgnoreAttributeOrder(true);
@@ -55,30 +54,8 @@ public class RIFParseAndSerializeTest extends XMLTestCase {
 		XMLUnit.setIgnoreDiffBetweenTextAndCDATA(true);
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
-	public void testParseAndSerializeXML_RIF_BLD() throws SAXException,
-			IOException, ParserConfigurationException {
-		String[] rifFiles = TestUtils.getRIFTestFiles("src/test/resources");
-		assertNotNull(rifFiles);
-
-		for (int i = 0; i < rifFiles.length; i++) {
-			parser = new XmlParser(true);
-			serializer = new XmlSerializer(true);
-
-			String fileName = rifFiles[i];
-			testParseAndSerializeXML_RIF_BLD(fileName);
-
-			parser = null;
-			serializer = null;
-		}
-	}
-
-	@Test
-	public void testSerializePresentationSyntax_RIF_BLD() throws SAXException,
+	public void testSerialize_RIF_BLD() throws SAXException,
 			IOException, ParserConfigurationException {
 		String[] rifFiles = TestUtils.getRIFTestFiles("src/test/resources");
 		assertNotNull(rifFiles);
@@ -94,31 +71,6 @@ public class RIFParseAndSerializeTest extends XMLTestCase {
 			parser = null;
 			presentationSerializer = null;
 		}
-	}
-
-	private void testParseAndSerializeXML_RIF_BLD(String fileName)
-			throws SAXException, IOException, ParserConfigurationException {
-		logger.debug("Testing " + fileName);
-
-		// Parse RIFDocument
-		Reader rifXmlFileReader = TestUtils.getFileReader(fileName);
-		assertNotNull("Test file " + fileName + " could not be found",
-				rifXmlFileReader);
-
-		Document rifDocument = parser.parseDocument(rifXmlFileReader);
-		assertNotNull(rifDocument);
-
-		// Serialize to XML
-		String serializedRIFXmlDocument = serializer.serialize(rifDocument);
-
-		// Compare to original
-		File file = new File(TestUtils.getFileURI(fileName));
-		String originalRIFXml = FileUtils.readFileToString(file);
-		originalRIFXml = originalRIFXml.replaceAll("ordered=\"yes\"", "");
-
-		assertXMLEqual(
-				"Serialized and original documents do not match for file "
-						+ fileName, originalRIFXml, serializedRIFXmlDocument);
 	}
 
 	private void testSerializePresentation_RIF_BLD(String fileName)
@@ -137,8 +89,8 @@ public class RIFParseAndSerializeTest extends XMLTestCase {
 		assertNotNull(rifDocument);
 
 		// Serialize to presentation syntax
-		presentationSerializer.visit(rifDocument);
-		String serializedRIF = presentationSerializer.getString()
+		String serializedRIF = presentationSerializer.serialize(rifDocument);
+		serializedRIF = presentationSerializer.getString()
 				.replace("\r", "").replace("\t", "").replace("\n", "")
 				.replace(" ", "").trim();
 
@@ -149,6 +101,5 @@ public class RIFParseAndSerializeTest extends XMLTestCase {
 
 		assertEquals("Serialized and original documents do not match for file "
 				+ fileName, originalRIF, serializedRIF);
-	}
-
+	}	
 }
