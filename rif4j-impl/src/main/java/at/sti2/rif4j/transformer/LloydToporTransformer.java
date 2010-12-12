ef3c341ab41697c5d82be2f1c822b75a71fefa60
adrian_marte <adrian_marte@1816c63b-b73a-4b72-92f7-1bc7269d86da>
2010-12-12 18:51:46 +0000

    Improved Lloyd-Topor transformation.

--- rif4j-impl/src/main/java/at/sti2/rif4j/transformer/LloydToporTransformer.java
+++ rif4j-impl/src/main/java/at/sti2/rif4j/transformer/LloydToporTransformer.java
@@ -1,20 +1,33 @@
+/*
+ * Copyright 2010 STI Innsbruck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package at.sti2.rif4j.transformer;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import at.sti2.rif4j.condition.AndFormula;
 import at.sti2.rif4j.condition.AtomicFormula;
-import at.sti2.rif4j.condition.ExistsFormula;
-import at.sti2.rif4j.condition.ExternalFormula;
 import at.sti2.rif4j.condition.Formula;
-import at.sti2.rif4j.condition.FormulaVisitor;
 import at.sti2.rif4j.condition.OrFormula;
 import at.sti2.rif4j.rule.ImpliesFormula;
 
 /**
  * <p>
- * Transforms {@link ImpliesFormula}s according to the following rules:
+ * Transforms {@link ImpliesFormula}s according to Lloyd-Topor transformation
+ * function <i>t</i> defined below:
  * </p>
  * <table cellspacing="1" border="1">
  * <tr>
@@ -22,12 +35,12 @@ import at.sti2.rif4j.rule.ImpliesFormula;
  * <td><b>Simplified rule(s)</b></td>
  * </tr>
  * <tr>
- * <td>H<sub>1</sub> and ... and H<sub>n</sub> :- B</td>
- * <td>H<sub>1</sub> :- B, ..., H<sub>n</sub> :- B</td>
+ * <td>t(H<sub>1</sub> and ... and H<sub>n</sub> :- B .)</td>
+ * <td>t(H<sub>1</sub> :- B .), ..., t(H<sub>n</sub> :- B .)</td>
  * </tr>
  * <tr>
- * <td>H :- B<sub>1</sub> or ... or B<sub>n</sub></td>
- * <td>H :- B<sub>1</sub>, ..., H :- B<sub>n</sub></td>
+ * <td>t(H :- B<sub>1</sub> or ... or B<sub>n</sub> .)</td>
+ * <td>t(H :- B<sub>1</sub> .), ..., t(H :- B<sub>n</sub> .)</td>
  * </tr>
  * </table>
  * </p>
@@ -37,29 +50,60 @@ import at.sti2.rif4j.rule.ImpliesFormula;
 public class LloydToporTransformer {
 
 	public List<ImpliesFormula> transform(final ImpliesFormula implies) {
-		// Create a new implies formula of the form H :- Bi for each atom Bi in
-		// a OrFormula.
-		BodyVisitor bodyVisitor = new BodyVisitor(implies);
-		implies.getBody().accept(bodyVisitor);
+		List<ImpliesFormula> eraseOrformulas = eraseOrFormulaInBody(implies);
 
-		List<ImpliesFormula> eraseOrformulas = bodyVisitor.newImpliesFormulas;
+		if (eraseOrformulas.size() > 0) {
+			return transform(eraseOrformulas);
+		}
 
-		List<ImpliesFormula> eraseAndformulas = new ArrayList<ImpliesFormula>();
+		List<ImpliesFormula> eraseAndformulas = eraseAndFormulaInHead(implies);
 
-		for (ImpliesFormula impliesFormula : eraseOrformulas) {
+		if (eraseAndformulas.size() > 0) {
+			return transform(eraseAndformulas);
+		}
+
+		return Collections.singletonList(implies);
+	}
+
+	public List<ImpliesFormula> transform(List<ImpliesFormula> implies) {
+		List<ImpliesFormula> resultingFormulas = new ArrayList<ImpliesFormula>();
+
+		for (ImpliesFormula formula : implies) {
+			resultingFormulas.addAll(transform(formula));
+		}
+
+		return resultingFormulas;
+	}
+
+	private static List<ImpliesFormula> eraseAndFormulaInHead(
+			ImpliesFormula impliesFormula) {
+		List<ImpliesFormula> newFormulas = new ArrayList<ImpliesFormula>();
+
+		if (impliesFormula.getHead().size() > 1) {
 			// Create a new implies formula of the form Hi :- B for each head
 			// atom Hi.
 			for (AtomicFormula headAtom : impliesFormula.getHead()) {
-				ImpliesFormula newImplies = new ImpliesFormula(impliesFormula
-						.getBody(), headAtom);
-				eraseAndformulas.add(newImplies);
+				ImpliesFormula newImplies = new ImpliesFormula(
+						impliesFormula.getBody(), headAtom);
+				newFormulas.add(newImplies);
 			}
 		}
 
-		return eraseAndformulas;
+		return newFormulas;
 	}
 
-	private static class BodyVisitor implements FormulaVisitor {
+	private static List<ImpliesFormula> eraseOrFormulaInBody(
+			ImpliesFormula impliesFormula) {
+		// Create a new implies formula of the form H :- Bi for each atom Bi in
+		// a OrFormula.
+		BodyVisitor bodyVisitor = new BodyVisitor(impliesFormula);
+		impliesFormula.getBody().accept(bodyVisitor);
+
+		List<ImpliesFormula> eraseOrformulas = bodyVisitor.newImpliesFormulas;
+		return eraseOrformulas;
+	}
+
+	private static class BodyVisitor extends DefaultFormulaVisitor {
 
 		public List<ImpliesFormula> newImpliesFormulas = new ArrayList<ImpliesFormula>();
 
@@ -67,12 +111,6 @@ public class LloydToporTransformer {
 
 		public BodyVisitor(ImpliesFormula impliesFormula) {
 			this.impliesFormula = impliesFormula;
-		}
-
-		@Override
-		public void visit(AndFormula andFormula) {
-			// Do not change the rule.
-			newImpliesFormulas.add(impliesFormula);
 		}
 
 		@Override
@@ -84,24 +122,6 @@ public class LloydToporTransformer {
 						head);
 				newImpliesFormulas.add(newImplies);
 			}
-		}
-
-		@Override
-		public void visit(ExistsFormula existsFormula) {
-			// Do not change the rule.
-			newImpliesFormulas.add(impliesFormula);
-		}
-
-		@Override
-		public void visit(ExternalFormula externalFormula) {
-			// Do not change the rule.
-			newImpliesFormulas.add(impliesFormula);
-		}
-
-		@Override
-		public void visit(AtomicFormula atomicFormula) {
-			// Do not change the rule.
-			newImpliesFormulas.add(impliesFormula);
 		}
 
 	}
