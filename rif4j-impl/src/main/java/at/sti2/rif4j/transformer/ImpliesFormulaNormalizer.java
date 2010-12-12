7ba0fbf1b4644d253919c0d81d10a48180e65aa3
adrian_marte <adrian_marte@1816c63b-b73a-4b72-92f7-1bc7269d86da>
2010-12-12 18:52:58 +0000

    Implemented normalization of formulas (not complete yet).

---
+++ rif4j-impl/src/main/java/at/sti2/rif4j/transformer/ImpliesFormulaNormalizer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2010 STI Innsbruck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package at.sti2.rif4j.transformer;
+
+import at.sti2.rif4j.condition.Formula;
+import at.sti2.rif4j.rule.ImpliesFormula;
+
+/**
+ * @author Adrian Marte
+ */
+public class ImpliesFormulaNormalizer {
+
+	public ImpliesFormula normalize(ImpliesFormula impliesFormula) {
+		// Normalize the body of an ImpliesFormula.
+		Formula body = impliesFormula.getBody();
+
+		FormulaNormalizer normalizer = new FormulaNormalizer();
+		Formula normalizedBody = normalizer.normalize(body);
+
+		impliesFormula.setBody(normalizedBody);
+
+		return impliesFormula;
+	}
+
+}
