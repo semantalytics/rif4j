cb8b1fe4196a264cb52f0e66d8682b9ba6b857ec
adrian_marte <adrian_marte@1816c63b-b73a-4b72-92f7-1bc7269d86da>
2010-12-15 13:07:29 +0000

    Parameterized entailment tests.

---
+++ rif4j-iris/src/test/java/at/sti2/rif4j/reasoner/iris/AbstractEntailmentTest.java
@@ -0,0 +1,79 @@
+package at.sti2.rif4j.reasoner.iris;
+
+import java.io.FileNotFoundException;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import at.sti2.rif4j.TestUtils;
+import at.sti2.rif4j.condition.Formula;
+import at.sti2.rif4j.rule.Document;
+
+@RunWith(Parameterized.class)
+public abstract class AbstractEntailmentTest {
+
+	protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+	private static final String PREMISE_SUFFIX = "-premise";
+
+	private static final String CONCLUSION_SUFFIX = "-conclusion";
+
+	private static final String NONCONCLUSION_SUFFIX = "-nonconclusion";
+
+	private static final String FILE_EXTENSION = ".rif";
+
+	private IrisRifReasoner reasoner;
+
+	private String testDirectory;
+
+	private String testCaseName;
+
+	private boolean expectedEvaluation;
+
+	public AbstractEntailmentTest(String testDirectory, String testCaseName,
+			boolean expectedEvaluation) {
+		this.testDirectory = testDirectory;
+		this.testCaseName = testCaseName;
+		this.expectedEvaluation = expectedEvaluation;
+	}
+
+	@Before
+	public void setUp() {
+		reasoner = new IrisRifReasoner();
+	}
+
+	@Test
+	public void testEntailment() throws FileNotFoundException {
+		logger.debug("Testing positive entailment of " + testCaseName);
+
+		String premiseFileName = testCaseName + PREMISE_SUFFIX + FILE_EXTENSION;
+		String premisePath = testDirectory + testCaseName + "/"
+				+ premiseFileName;
+
+		String conclusionSuffix = expectedEvaluation ? CONCLUSION_SUFFIX
+				: NONCONCLUSION_SUFFIX;
+
+		String conclusionFileName = testCaseName + conclusionSuffix
+				+ FILE_EXTENSION;
+		String conclusionPath = testDirectory + testCaseName + "/"
+				+ conclusionFileName;
+
+		Document document = TestUtils.parseDocument(premisePath);
+		
+		System.out.println(document);
+		
+		Formula formula = TestUtils.parseFormula(conclusionPath);
+
+		boolean entails = reasoner.entails(document, formula);
+		logger.debug("Result is: " + entails);
+
+		Assert.assertEquals(testCaseName, expectedEvaluation, entails);
+	}
+
+}
