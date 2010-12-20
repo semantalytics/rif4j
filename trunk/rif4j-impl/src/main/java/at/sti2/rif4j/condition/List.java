275dceeb560159e4b5bc4a75b7c4d629270b2139
adrian_marte <adrian_marte@1816c63b-b73a-4b72-92f7-1bc7269d86da>
2010-12-20 11:08:18 +0000

    A List can now be open or closed.

--- trunk/rif4j-impl/src/main/java/at/sti2/rif4j/condition/List.java
+++ trunk/rif4j-impl/src/main/java/at/sti2/rif4j/condition/List.java
@@ -22,20 +22,66 @@ import at.sti2.rif4j.Assertions;
 import at.sti2.rif4j.serializer.presentation.PresentationSerializer;
 
 /**
+ * <p>
+ * There are two kinds of list terms: open and closed.
+ * </p>
+ * <p>
+ * A closed list has the form List(t1 ... tm), where m>0 and t1, ..., tm are
+ * terms. An open list (or a list with a tail) has the form OpenList(t1 ... tm
+ * t), where m>0 and t1, ..., tm, t are terms. Open lists are usually written
+ * using the following: List(t1 ... tm | t).
+ * </p>
+ * 
  * @author Adrian Marte
  */
 public class List extends AbstractDescribable implements Term {
 
+	private boolean isOpen;
+
 	private java.util.List<Term> elements;
 
+	/**
+	 * Creates an empty closed {@link List}.
+	 */
 	public List() {
 		elements = new ArrayList<Term>();
 	}
 
+	/**
+	 * Creates a closed {@link List} for the specified elements.
+	 * 
+	 * @param elements
+	 *            The elements of the list.
+	 */
 	public List(java.util.List<Term> elements) {
 		Assertions.notNull("elements", elements);
 
 		this.elements = elements;
+	}
+
+	/**
+	 * Returns <code>true</code> if this is an open list. An open list (or a
+	 * list with a tail) has the form OpenList(t1 ... tm t), where m>0 and t1,
+	 * ..., tm, t are terms. Returns <code>false</code> per default.
+	 * 
+	 * @returns <code>true</code> if this is an open list, <code>false</code>
+	 *          otherwise.
+	 */
+	public boolean isOpen() {
+		return isOpen;
+	}
+
+	/**
+	 * Determines if this is a open list. An open list (or a list with a tail)
+	 * has the form OpenList(t1 ... tm t), where m>0 and t1, ..., tm, t are
+	 * terms.
+	 * 
+	 * @param isOpen
+	 *            <code>true</code> if this is an open list, <code>false</code>
+	 *            otherwise.
+	 */
+	public void setOpen(boolean isOpen) {
+		this.isOpen = isOpen;
 	}
 
 	public java.util.List<Term> getElements() {
@@ -46,6 +92,29 @@ public class List extends AbstractDescribable implements Term {
 		Assertions.notNull("elements", elements);
 
 		this.elements = elements;
+	}
+
+	public List flatten() {
+		java.util.List<Term> newElements = new ArrayList<Term>();
+
+		for (Term element : elements) {
+			if (element instanceof List) {
+				List elementList = (List) element;
+				newElements.addAll(elementList.flatten().getElements());
+			} else {
+				newElements.add(element);
+			}
+		}
+
+		if (!elements.equals(newElements)) {
+			List newList = new List(newElements);
+			newList.setId(getId());
+			newList.setMetadata(getMetadata());
+
+			return newList;
+		}
+
+		return this;
 	}
 
 	@Override
