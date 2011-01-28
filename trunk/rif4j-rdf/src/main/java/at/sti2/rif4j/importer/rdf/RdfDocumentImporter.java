32d3586b7e5ddd697197c1570ea6448300f01cc3
adrian_marte <adrian_marte@1816c63b-b73a-4b72-92f7-1bc7269d86da>
2011-01-28 10:12:02 +0000

    Organized imports.

--- trunk/rif4j-rdf/src/main/java/at/sti2/rif4j/importer/rdf/RdfDocumentImporter.java
+++ trunk/rif4j-rdf/src/main/java/at/sti2/rif4j/importer/rdf/RdfDocumentImporter.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
