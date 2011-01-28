55fd25493046c9b8094b251a835537d5be2f30db
adrian_marte <adrian_marte@1816c63b-b73a-4b72-92f7-1bc7269d86da>
2011-01-28 10:06:34 +0000

    Added class DocumentImporter.

---
+++ trunk/rif4j-impl/src/main/java/at/sti2/rif4j/importer/DocumentImporter.java
@@ -0,0 +1,13 @@
+package at.sti2.rif4j.importer;
+
+import java.net.URI;
+
+import at.sti2.rif4j.rule.Document;
+
+public interface DocumentImporter {
+
+	public boolean supports(URI profile);
+
+	public Document importDocument(URI uri, URI profile) throws ImportException;
+
+}
