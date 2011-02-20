70dfa601e99b293afcc65fdb96f2c75f4410bbda
adrian_marte <adrian_marte@1816c63b-b73a-4b72-92f7-1bc7269d86da>
2011-02-20 18:54:19 +0000

    Implemented a DocumentManager.

---
+++ trunk/rif4j-impl/src/main/java/at/sti2/rif4j/manager/DocumentLoadingException.java
@@ -0,0 +1,26 @@
+package at.sti2.rif4j.manager;
+
+public class DocumentLoadingException extends Exception {
+
+	/**
+	 * Generated serial version UID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DocumentLoadingException() {
+		super();
+	}
+
+	public DocumentLoadingException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DocumentLoadingException(String message) {
+		super(message);
+	}
+
+	public DocumentLoadingException(Throwable cause) {
+		super(cause);
+	}
+
+}
