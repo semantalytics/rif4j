e6b9dfeee8757e119a773cba464361e976b4d665
adrian_marte <adrian_marte@1816c63b-b73a-4b72-92f7-1bc7269d86da>
2011-03-28 07:34:11 +0000

    Ignore the conformance tests.

--- trunk/rif4j-integration-test/src/test/java/at/sti2/rif4j/reasoner/iris/rdb/LocalPositiveEntailmentIT.java
+++ trunk/rif4j-integration-test/src/test/java/at/sti2/rif4j/reasoner/iris/rdb/LocalPositiveEntailmentIT.java
@@ -3,11 +3,13 @@ package at.sti2.rif4j.reasoner.iris.rdb;
 import java.net.URI;
 
 import org.junit.After;
+import org.junit.Ignore;
 
 import at.sti2.rif4j.reasoner.AbstractLocalPositiveEntailmentTest;
 import at.sti2.rif4j.reasoner.Reasoner;
 import at.sti2.rif4j.reasoner.ReasonerFactory;
 
+@Ignore
 public class LocalPositiveEntailmentIT extends
 		AbstractLocalPositiveEntailmentTest {
 
